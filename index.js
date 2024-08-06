aedbeb0d97897a76905d4d6837cca00ae3204b34
Dushyant Verma <167190841+dushyant-verma@users.noreply.github.com>
2024-08-06 23:53:34 +0530

    Trying to push the code

---
+++ index.js
@@ -0,0 +1,18 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+
+    try {
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+
+    } catch (e) {
+        document.getElementById('display').value = 'Error';
+    }
+};
